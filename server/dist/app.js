0e394ade8c3f228cb1b12d0c29eb21b8efd9d811
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-03 20:05:16 +0100

    commit

--- server/dist/app.js
+++ server/dist/app.js
@@ -4,12 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use((0, cors_1.default)());
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
 });
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+server.listen(3001, () => {
+    console.log('SERVER IS RUNNING');
 });
-//# sourceMappingURL=app.js.map
+// import express from 'express';
+// const app = express();
+// const port = 3000;
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+// app.listen(port, () => {
+//   return console.log(`Express is listening at http://localhost:${port}`);
+// });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,32 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
 
-import express from 'express';
-const app = express();
-const port = 3000;
+const app = express()
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const server = http.createServer(app)
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
+
+server.listen(3001, () => {
+  console.log('SERVER IS RUNNING')
+})
+
+// import express from 'express';
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   return console.log(`Express is listening at http://localhost:${port}`);
+// });
