ecc5d875f11c6ed34d8ba77f572fe1cfed73dc4d
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-06 19:24:07 +0100

    commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,22 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
-
-io.on('connection', (socket:any) => {
-  console.log(`User Connected: ${socket.id}`);
-});
-
+    methods: ['GET', 'POST']
+  }
+})
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`)
+})
 server.listen(3001, () => {
-  console.log('SERVER IS RUNNING!!');
-});
+  console.log('SERVER IS RUNNING')
+})
