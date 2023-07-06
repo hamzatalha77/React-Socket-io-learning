5c8d702b9ade05e98ba349b7abf4185ef16b5e9d
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-07 00:39:32 +0100

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import io, { Socket } from 'socket.io-client'
 
 const socket: Socket = io('http://localhost:3001')
 const App = () => {
+  const [message, setMessage] = useState('')
+  const [messageReceive, setMessageReceive] = useState('')
   const sendMessage = () => {
-    // socket.emit()
+    socket.emit('send_message', { message })
   }
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageReceive(data.message)
+    })
+  }, [socket])
   return (
     <div className="App">
-      <input placeholder="Message..." />
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value)
+        }}
+      />
       <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      <p>{messageReceive}</p>
     </div>
   )
 }
