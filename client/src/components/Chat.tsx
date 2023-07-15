e997341b949f1203a86fafcc780c7604e47bb47b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-15 12:40:25 +0100

    commit

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface ChatProps {
   socket: any
@@ -7,6 +7,20 @@ interface ChatProps {
 }
 
 const Chat: React.FC<ChatProps> = ({ socket, username, room }) => {
+  const [currentMessage, setCurrentMessage] = useState('')
+  const sendMessage = async () => {
+    if (currentMessage !== '') {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ':' +
+          new Date(Date.now()).getMinutes()
+      }
+    }
+  }
   return (
     <div>
       <div className="chat-header">
@@ -14,8 +28,14 @@ const Chat: React.FC<ChatProps> = ({ socket, username, room }) => {
       </div>
       <div className="chat-body"></div>
       <div className="chat-footer">
-        <input type="text" placeholder="hey..." />
-        <button>&#9658;</button>
+        <input
+          type="text"
+          placeholder="hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value)
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
   )
